7766dcdc1467b50d4e3da151ffe7a6c724d4d9b7
Katie Dessain <JillSandwich@example.com>
2021-08-12 16:34:10 -0500

    finished most of the exercise but not all

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -14,6 +14,14 @@ export class AppComponent {
   message = 'Space shuttle ready for takeoff!';
   takeOffEnabled: boolean = true;
 
+  gutterCheck(width, height) {
+    if (width > 260000 || width < 0 || height < 0 || height > 340000) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
+    }
+   }
+
   handleTakeOff() {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
     if (result) {
@@ -21,26 +29,33 @@ export class AppComponent {
        this.height = 10000;
        this.width = 0;
        this.message = 'Shuttle in flight.';
+       this.takeOffEnabled = false;
     }
  }
 
- handleLand() {
+ handleLand(rocketImage) {
   let result = window.confirm('Are you sure you want to land the shuttle?');
   if (result) {
      this.color = 'green';
      this.height = 10000;
      this.width = 0;
      this.message = 'Shuttle landed.';
+     this.takeOffEnabled = true;
+     rocketImage.style.bottom = '0px';
+     rocketImage.style.left = '0px';
   }
  }
 
- handleAbort() {
+ handleAbort(rocketImage) {
   let result = window.confirm('Are you sure you want to abort?');
   if (result) {
      this.color = 'red';
      this.height = 10000;
      this.width = 0;
      this.message = 'ABORTED MISSION.';
+     this.takeOffEnabled = true;
+     rocketImage.style.bottom = '0px';
+     rocketImage.style.left = '0px';
   }
  }
  
@@ -65,6 +80,7 @@ export class AppComponent {
     rocketImage.style.bottom = movement;
     this.width = this.width - 10000;
   }
+  this.gutterCheck(this.width, this.height);
  }
 
 }
